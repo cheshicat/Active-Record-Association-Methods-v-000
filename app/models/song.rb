60727ed111b26cc7ac14b030ef7c1197423dc6f8
Allison Howard <alliemhow@gmail.com>
2019-01-13 18:22:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    self.genres.collect {|genre| genre.name}
+    self.genre.collect {|genre| genre.name}
   end
 
   def drake_made_this
